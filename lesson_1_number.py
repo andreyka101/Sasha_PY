8656624594514dbaa55d6074cb47fad7d936a939
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-08 15:07:04 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,113 @@
+# print("hello world")
+
+
+
+# num = 2
+# print(num)
+
+
+
+# num_int = 8
+# num_float = 8.4
+# num_bool = True
+# num_str = "77 777"
+
+
+
+# print = 5
+# print(print)
+# num1 = 0
+# print(num1)
+# hello_world = 0
+# print(hello_world)
+# helloWorld = 0
+# print(helloWorld)
+
+
+
+# print(4 + 4)
+# print(4 - 4)
+# print(4 * 4)
+# print(4 ** 4)
+# print(4 / 4)
+
+
+
+# num_1 = 5
+# print(num_1)
+# num_1 = num_1 + num_1
+# print(num_1)
+
+
+
+# str_1 = "5"
+# print(str_1 * 4)
+# str_1 = int(str_1)
+# print(str_1 * 4)
+
+
+
+# print(type(5))
+# print(type(5.1))
+# print(type("5"))
+
+
+
+# str_2 = "5"
+# print(type(str_2))
+# str_2 = float(str_2)
+# print(type(str_2))
+# print(str_2)
+
+
+
+# print(float(4))
+# print(int(4.9))
+
+
+
+# print("w",3,6,"-")
+
+
+
+# num_inp = input()
+# print(num_inp)
+# print(type(num_inp))
+
+
+
+# num_inp = input("введите число --")
+# num_inp = int(num_inp)
+# print(type(num_inp))
+# print(num_inp * 3)
+
+
+
+# num_inp = int(input("введите число --"))
+# print(type(num_inp))
+# print(int(input("введите число --")) * 3)
+
+
+
+# print("y" + "hi")
+# num_3 = 8
+# str_3 = "ответ: " + str(num_3)
+# print(str_3)
+
+# str_5 = "llo"
+# str_4 = f"ответ: {num_3}  he{str_5} {1}"
+# print(str_4)
+
+
+
+# print("1", " ", "p")
+
+
+
+# str_6 = "привет"
+# print(str_6[0])
+# print(str_6[4])
+
+# print(str_6[4] + str_6[1])
+
+
