f0ff2e842d0db3001ea05a5dca98d8f006dd81a2
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-07 16:01:46 +0300

    rgrgrggg

--- lesson_16_arrey.py
+++ lesson_16_arrey.py
@@ -9,9 +9,9 @@ from array import array
 # array(r , s)
 # r - режим массива (размер каждой ячейки в массиве , таблица в ссылке)
 # s - список (можно не писать)
-arr = array("b" , [2 , 4])
-print(arr)
-print(arr[0])
+arr = array("b" , [2 , 4 ,7 , 1 , 3 , 5 , 8])
+# print(arr)
+# print(arr[0])
 
 
 
@@ -28,6 +28,68 @@ print(arr[0])
 
 
 
+# print(arr.typecode)
 
+
+
+# print(arr.itemsize)
+
+
+
+# print(arr.buffer_info())
+
+
+
+# print(arr)
+# arr.byteswap()
+# print(arr)
+
+
+
+# print(arr.tobytes())
+
+
+
+# arr.frombytes(b'\x20\x49\x87\x11')
+# arr.frombytes(bytes("\x20\x49\x87\x11" , encoding = "UTF-8"))
+# print(arr)
+
+
+
+# with open("arr_text.txt" , "bw") as file:
+#     arr.tofile(file)
+
+
+
+# with open("arr_text.txt" , "br") as file:
+#     arr_2 = array("b")
+#     arr_2.fromfile(file , 4)
+#     print(arr_2)
+
+
+
+# arr.fromlist([2,5,80])
+# print(arr)
+
+
+
+# arr.fromlist([2,5,80])
+# print(arr)
+
+
+
+# arr_list = arr.tolist()
+# print(type(arr_list))
+
+# arr_3 = array("b" , arr_list)
+# print(arr_3)
+
+
+
+
+
+el = arr[0]
+arr[0] = arr[1]
+arr[1] == el
 
 
