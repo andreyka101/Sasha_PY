3e71880b77a3d1d6cd2a155f8e53d6989484c42a
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-29 17:28:08 +0300

    цвцвцв

--- lesson_22_menu.py
+++ lesson_22_menu.py
@@ -14,20 +14,27 @@ num_menu.add_command(label="n3")
 
 def fun():
     window.config(bg="#4e4e4e")
+
+# создание главного меню
 main_menu = Menu()
+# кнопка для меню
 main_menu.add_command(label="123" , command=fun)
 
 
+# создание меню
 file_menu = Menu(tearoff=0)
 
+# кнопка для меню
 file_menu.add_command(label="sive")
 
+# checkbox
 num_check = IntVar()
 file_menu.add_checkbutton(label="check" ,variable=num_check)
 
+# разделение
 file_menu.add_separator()
 
-
+# radiobutton
 def fun_2():
     print(num_radio.get())
 num_radio = StringVar()
@@ -35,7 +42,7 @@ file_menu.add_radiobutton(label="radio1" , variable=num_radio , value= "b1" , co
 file_menu.add_radiobutton(label="radio2" , variable=num_radio , value= "b2" , command=fun_2)
 file_menu.add_radiobutton(label="radio3" , variable=num_radio , value= "b3" , command=fun_2)
 
-
+# создание под меню
 file_menu.add_cascade(menu=num_menu , label="num" , command=fun)
 
 
@@ -45,6 +52,7 @@ main_menu.add_cascade(label="file" , menu=file_menu)
 main_menu.add_cascade(label="num" , menu=num_menu)
 
 
+# подключение главного меню
 window.config(menu=main_menu)
 
 window.mainloop()
--- lesson_23_time.py
+++ lesson_23_time.py
@@ -1,5 +1,9 @@
+
+# подключаем библитеку time
 import time
+# .time() - получить unix время
 print(time.time())
+# .sleep(s) - программа останавливается на s секунд
 time.sleep(3)
 print(time.time())
 
